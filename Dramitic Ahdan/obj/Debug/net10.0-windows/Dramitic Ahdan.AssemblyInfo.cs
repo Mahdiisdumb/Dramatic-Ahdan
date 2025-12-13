ef7cddea998ee65fa1c8edb99fce2fba1d14d9a4
Mahdiisdumb <mahdihamade6789010@outlook.com>
2025-12-13 12:03:52 -0500

    d

--- Dramitic Ahdan/obj/Debug/net10.0-windows/Dramitic Ahdan.AssemblyInfo.cs
+++ Dramitic Ahdan/obj/Debug/net10.0-windows/Dramitic Ahdan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dramitic Ahdan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab8efd0c1b365a81cbc193e8206a8346e27b9a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da4246e76160b48c82601935b197a7ddd5bc3ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dramitic Ahdan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dramitic Ahdan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
